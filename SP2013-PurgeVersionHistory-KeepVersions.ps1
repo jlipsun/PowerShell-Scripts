7dc4d03e577c5362d94a1421c7264ae82374d3ab
jlipsun <jethrolipsun@gmail.com>
2019-05-07 22:13:07 -0700

    Update SP2013-PurgeVersionHistory-KeepVersions.ps1
    In SharePoint 2013, you can enable version history and see all of the different versions of the documents. I had a request to first purge version history older than 6 months and for any versions that remain, they would want to keep only 20 versions. Since there were many documents in the library, I created a script to run once and to meet this requirement.

--- SP2013-PurgeVersionHistory-KeepVersions.ps1
+++ SP2013-PurgeVersionHistory-KeepVersions.ps1
@@ -1,3 +1,8 @@
+#In SharePoint 2013, you can enable version history and see all of the different versions of the documents. 
+#I had a request to first purge version history older than 6 months and for any versions that remain, 
+#they would want to keep only 20 versions. Since there were many documents in the library, I created this script to run once and 
+#to meet this requirement.
+
 $web = get-spweb "SP2013-SiteCollection-URL"
 $doclib = $web.Lists["SP2013-List"]
 $startdate = get-date
